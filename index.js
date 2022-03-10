2bf71f6cbe5aab4672144d7d78ba2da12cf10a0e
Jade Kimbrough <jadekimbrough123@gmail.com>
2022-03-10 20:53:36 +0000

    adds bootstrap and content to pages

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine','ejs');
+
+//this will allow us to serve up static files, CSS, images & JS
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+	var title = 'Home Page';
+  res.render('pages/index',{title:title});
+});
+
+// about us
+app.get('/my-dog', (req, res) => {
+	var title = 'My Dog';
+  res.render('pages/my-dog',{title:title});
+});
+
+//recipe
+app.get('/recipe', (req, res) => {
+	var title = 'Scalloped Potato Recipe';
+  res.render('pages/recipe',{title:title});
+});
+
+//stickers
+app.get('/sticker', (req, res) => {
+	var title = 'My Stickers';
+  res.render('pages/sticker',{title:title});
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
