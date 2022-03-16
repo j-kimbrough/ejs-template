66f68c065773f597c85bb356a895ad9bd44a26b0
Jade Kimbrough <jadekimbrough123@gmail.com>
2022-03-16 15:35:13 +0000

    adds users data and links to users data

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+var data = require('./data/test.json');
+
 app.set('view engine','ejs');
 
 //this will allow us to serve up static files, CSS, images & JS
@@ -19,6 +21,25 @@ app.get('/my-dog', (req, res) => {
   res.render('pages/my-dog',{title:title});
 });
 
+//add users route
+app.get('/users', function(req, res) {
+	var title = 'Users Page';
+	res.render('users/index', {
+    	title: title,
+    	users: data
+	});
+});
+
+//add user/view route - we are cheating by using the array index - 1
+app.get('/users/view/:id', function(req, res) {
+	var title = 'User Page';
+	var id = req.params.id;
+	res.render('users/view', {
+	title: title,
+	user: data[--id]
+	});
+});
+
 //recipe
 app.get('/recipe', (req, res) => {
 	var title = 'Scalloped Potato Recipe';
@@ -31,7 +52,6 @@ app.get('/sticker', (req, res) => {
   res.render('pages/sticker',{title:title});
 });
 
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(data);
 });
